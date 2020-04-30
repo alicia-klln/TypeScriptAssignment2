efeaf311f81b22074e3bc7418b4aa4ffbb3c167f
alicia-klln <57350795+alicia-klln@users.noreply.github.com>
2020-04-30 23:06:18 +0200

    zufallszahlen eingefügt

--- firsttry.js
+++ firsttry.js
@@ -13,36 +13,103 @@ for (var i = 0;i < rightSide.length; i++) {
 button1 = document.getElementById("btn1");
 button2 = document.getElementById("btn2");
 button3 = document.getElementById("btn3");
-button4 = document.getElementById("btn4");
 
-berechnet1 = document.getElementById("berechnet1");
-berechnet2 = document.getElementById("berechnet2");
-berechnet3 = document.getElementById("berechnet3");
-berechnet4 = document.getElementById("berechnet4");
+berechnet1 = document.getElementById("ergebnisInput1");
+berechnet2 = document.getElementById("ergebnisInput2");
+berechnet3 = document.getElementById("ergebnisInput3");
 
 button1.addEventListener('click', addiereZahlen1);
 button2.addEventListener('click', addiereZahlen2);
 button3.addEventListener('click', addiereZahlen3);
-button4.addEventListener('click', addiereZahlen4);
 
 function addiereZahlen1 (){
-    const myInput1l = parseInt(document.getElementById("input1l").value);
-    const myInput1r = parseInt(document.getElementById("input1r").value);
-    const ergebnis1 = myInput1l + myInput1r;
-    console.log(ergebnis1);
-    berechnet1.innerHTML += " " + ergebnis1;
+    var clicked;
+    clicked = 0;
+    console.log(clicked);
+
+    if (clicked < 1) {
+        const myInput1l = parseInt(document.getElementById("input1l").value);
+        const myInput1r = parseInt(document.getElementById("input1r").value);
+        const ergebnis1 = myInput1l + myInput1r;
+        clicked++;
+        console.log(ergebnis1);
+        berechnet1.value = ergebnis1;
+    }else {
+        berechnet1.value = "";
+        }
 }
+ 
 function addiereZahlen2 (){
-    const myInput2l = parseInt(document.getElementById("input2l").value);
-    const myInput2r = parseInt(document.getElementById("input2r").value);
-    const ergebnis2 = myInput2l + myInput2r;
-    console.log(ergebnis2);
-    berechnet2.innerHTML += " " + ergebnis2;
-}
+    var clicked;
+    clicked = 0;
+    console.log(clicked);
+
+    if (clicked < 1) {
+        const myInput2l = parseInt(document.getElementById("input2l").value);
+        const myInput2r = parseInt(document.getElementById("input2r").value);
+        const ergebnis2 = myInput2l + myInput2r;
+        clicked++;
+        console.log(ergebnis2);
+        berechnet2.value = ergebnis2;
+    }else {
+        berechnet2.value = "";
+    }
+} 
+
 function addiereZahlen3 (){
-    const myInput3l = parseInt(document.getElementById("input3l").value);
-    const myInput3r = parseInt(document.getElementById("input3r").value);
-    const ergebnis3 = myInput3l + myInput3r;
-    console.log(ergebnis3);
-    berechnet3.innerHTML += " " + ergebnis3;
+    var clicked;
+    clicked = 0;
+    console.log(clicked);
+
+    if (clicked < 1) {
+        const myInput3l = parseInt(document.getElementById("input3l").value);
+        const myInput3r = parseInt(document.getElementById("input3r").value);
+        const ergebnis3 = myInput3l + myInput3r;
+        clicked++;
+        console.log(ergebnis3);
+        berechnet3.value = ergebnis3;
+    }else {
+        berechnet3.value = "";
 }
+}
+
+deleteAll = document.getElementById("deleteAll");
+
+deleteAll.addEventListener('click', deletedAll);
+
+function deletedAll() {
+    berechnet1.value = "";  
+    berechnet2.value = "";  
+    berechnet3.value = "";   
+}
+
+//Zufallszahl würfeln
+
+wuerfelButton = document.getElementById("wuerfelbutton");
+ausgabeZahl1 = document.getElementById("ausgabezahl1");
+ausgabeZahl2 = document.getElementById("ausgabezahl2");
+
+wuerfelButton.addEventListener('click', wuerfeln);
+
+function wuerfeln () {
+    var zufallsZahl1;
+    var zufallsZahl2;
+    zufallsZahl1 = Math.floor(Math.random()* 6) + 1;
+    zufallsZahl2 = Math.floor(Math.random()* 6) + 1;
+    console.log(zufallsZahl1);
+    ausgabeZahl1.value = zufallsZahl1;
+    ausgabeZahl2.value = zufallsZahl2;
+    
+    if (zufallsZahl1 = zufallsZahl2){
+        const gewinnBotschaft = document.getElementById("gewinnbotschaft");
+        gewinnBotschaft.innerHTML = "Super, du hast zwei gleiche Zahlen gewürfelt";
+        console.log("Super, du hast zwei gleiche Zahlen gewürfelt");
+    } else {
+        gewinnBotschaft.innerHTML = "";
+    }
+
+}
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ button1!.addEventListener('click', addiereZahlen1);
 button2!.addEventListener('click', addiereZahlen2);
 button3!.addEventListener('click', addiereZahlen3);
 
-
 function addiereZahlen1 (){
     
     const myInput1l: number = parseInt(document.querySelector<HTMLInputElement>("#input1l")!.value);
@@ -20,18 +19,57 @@ function addiereZahlen1 (){
     const ergebnis1: number = myInput1l + myInput1r;
     console.log(ergebnis1);
     berechnet1!.innerHTML += " " + ergebnis1;
+    if (ergebnis1 > 100){
+        alert("Deine Zahl ist ja ganz schön groß");
+    }
 }
-function addiereZahlen2 (){
+
+function addiereZahlen2 (this: any){
+    
+
     const myInput2l = parseInt(document.querySelector<HTMLInputElement>("#input2l")!.value);
     const myInput2r = parseInt(document.querySelector<HTMLInputElement>("#input2r")!.value);
-    const ergebnis2 = myInput2l + myInput2r;
-    console.log(ergebnis2);
-    berechnet2!.innerHTML += " " + ergebnis2;
+    
+    var clicked: number;
+    clicked = 0;
+
+    if(clicked < 2){
+        clicked++;
+        var ergebnis2 = myInput2l + myInput2r;
+        ergebnis2 = parseInt(this.ergebnis2);
+        console.log(ergebnis2);
+        berechnet2!.innerHTML += " " + ergebnis2;
+    }
+    else{
+        document.getElementById("btn2")!.disabled = true; 
+    }
+    document.getElementById("btn2")!.disabled = true; 
 }
+
 function addiereZahlen3 (){
-    const myInput3l: number  = parseInt(document.querySelector<HTMLInputElement>("#input3l")!.value);
+    const myInput3l = parseInt(document.querySelector<HTMLInputElement>("#input3l")!.value);
     const myInput3r: number = parseInt(document.querySelector<HTMLInputElement>("#input3r")!.value);
     const ergebnis3: number = myInput3l + myInput3r;
-    console.log(ergebnis3);
-    berechnet3!.innerHTML += " " + ergebnis3;
+    if (myInput3l !< 0) {
+        alert("Bitte gib Zahlen ein");
+    } else {
+        console.log(ergebnis3);
+        berechnet3!.innerHTML += " " + ergebnis3;
+    }
 }
+const deleteAll = document.getElementById("deleteAll");
+
+deleteAll!.addEventListener('click', deletedAll);
+function deletedAll() {
+
+        document.getElementById("input1l")!.remove;   // Funktioniert noch nicht!
+        console.log("cleared");
+}
+var p_neu = document.createElement("p");
+var t_neu = document.createTextNode("Neuer Absatz");
+p_neu.appendChild(t_neu);
+
+
+document.getElementById("absatz")?.appendChild(p_neu);
+
+
