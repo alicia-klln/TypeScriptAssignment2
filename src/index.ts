ab7240185ac06aaba4c6ec79ffc95aa0c741b321
alicia-klln <57350795+alicia-klln@users.noreply.github.com>
2020-05-06 19:04:13 +0200

    Funktionen wieder zum Laufen gebracht

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ namebestaetigen!.addEventListener('click', uebergebeName);
 
 function uebergebeName() {
     const name = document.querySelector<HTMLInputElement>("#namenseingabe")!.value;
-    console.log(name);
-
+    const nameFertig: string = name + "!";
+    // console.log(name);
     var clicked: number;
     clicked = 0;
 
     if (clicked < 1) {
-        if (name === null) {
+        if (name === "") {
             alert("Gib bitte deinen Namen ein.");
         }else {
             var anrede = document.querySelector<HTMLElement>("#h2");
             var anrede2 = document.querySelector<HTMLElement>("#anrede2");
             clicked++;
-            anrede!.innerHTML = "Hallo " + name + "! Schön, dass du da bist!";
-            anrede2!.innerHTML = "Überprüfe deine Ergebnisse, " + name;
+            anrede!.innerHTML = "Hallo " + nameFertig + " Schön, dass du da bist!";
+            anrede2!.innerHTML = "Überprüfe deine Ergebnisse, " + nameFertig;
         }    
     }
 }
@@ -36,61 +36,51 @@ button3!.addEventListener('click', addiereZahlen3);
 
 //Berechnung 1. Zeile
 function addiereZahlen1 (){
-    var clicked;
+    var clicked: number;
     clicked = 0;
 
     if (clicked < 1) {
         const myInput1l: number = parseInt(document.querySelector<HTMLInputElement>("#input1l")!.value);
         const myInput1r: number = parseInt(document.querySelector<HTMLInputElement>("#input1r")!.value);
         const ergebnis1: number = myInput1l + myInput1r;
-        clicked++;
-        console.log("Ergebnis 1: " + ergebnis1);
-        let berechnet1Value: any = berechnet1!.value;
-        berechnet1Value = ergebnis1;
-
-    }else {
-        berechnet1!.value = "";
+        // console.log("Ergebnis 1: " + ergebnis1);
+        var berechnet1String = ergebnis1.toString();
+        berechnet1!.value = berechnet1String;
     }
 }
 //Berechnung 2. Zeile
 function addiereZahlen2 (){
     
-    var clicked;
+    var clicked: number;
     clicked = 0;
 
     if (clicked < 1) {
         const myInput2l: number = parseInt(document.querySelector<HTMLInputElement>("#input2l")!.value);
         const myInput2r: number = parseInt(document.querySelector<HTMLInputElement>("#input2r")!.value);
         const ergebnis2: number = myInput2l + myInput2r;
-        clicked++;
-        console.log("Ergebnis 2: " + ergebnis2);
-        let berechnet2Value: any = berechnet2!.value;
-        berechnet2Value = ergebnis2;
-    }else {
-        berechnet2!.value = "";
+        // console.log("Ergebnis 2: " + ergebnis2);
+        var berechnet2String = ergebnis2.toString();
+        berechnet2!.value = berechnet2String;
     }
 }
 //Berechnung 3. Zeile
 function addiereZahlen3 (){
-    var clicked;
+    var clicked: number;
     clicked = 0;
 
     if (clicked < 1) {
         const myInput3l: number = parseInt(document.querySelector<HTMLInputElement>("#input3l")!.value);
         const myInput3r: number = parseInt(document.querySelector<HTMLInputElement>("#input3r")!.value);
         const ergebnis3: number = myInput3l + myInput3r;
-        clicked++;
-        console.log("Ergebnis 3: " + ergebnis3);
-        var berechnet3Value: any = berechnet3!.value;
-        berechnet3Value = ergebnis3;
-    }else {
-        berechnet3Value = "";
+        // console.log("Ergebnis 3: " + ergebnis3);
+        var berechnet3String = ergebnis3.toString();
+        berechnet3!.value = berechnet3String;
     }
 }
 
 //Alle Inhalte löschen
-const deleteAll= document.querySelector<HTMLElement>("#deleteAll");
 
+const deleteAll= document.querySelector<HTMLElement>("#deleteAll");
 deleteAll!.addEventListener('click', deletedAll);
 
 function deletedAll() {
@@ -107,7 +97,7 @@ function deletedAll() {
 }
 
 
-//Zufallszahl würfeln
+//Zufallszahlen würfeln
 
 const wuerfelButton = document.querySelector<HTMLButtonElement>("#wuerfelbutton");
 var ausgabeZahl1 = document.querySelector<HTMLInputElement>("#ausgabezahl1");
@@ -130,7 +120,7 @@ function wuerfeln () {
     zufallsZahl2 = Math.floor(Math.random()* 6) + 1;
     zufallsZahl3 = Math.floor(Math.random()* 6) + 1;
 
-    console.log(zufallsZahl1 + " & " + zufallsZahl2 + " & " + zufallsZahl3);
+    // console.log(zufallsZahl1 + " & " + zufallsZahl2 + " & " + zufallsZahl3);
 
     var zufallsZahl1String = zufallsZahl1.toString();
     ausgabeZahl1!.value = zufallsZahl1String;
@@ -142,8 +132,6 @@ function wuerfeln () {
     ausgabeZahl3!.value = zufallsZahl3String;
 
     wurf++;
-    // console.log("Zahl1 " + zufallsZahl1);
-    // ausgabeZahl1 = zufallsZahl1;
         
     if (zufallsZahl1 === zufallsZahl2 || 
         zufallsZahl1 === zufallsZahl3 ||  
@@ -154,10 +142,9 @@ function wuerfeln () {
     if (zufallsZahl1 === zufallsZahl2 && zufallsZahl1 === zufallsZahl3 && zufallsZahl2 === zufallsZahl3){
         dreierPasch+= 1;  
         console.log("Super, sogar ein Dreierpasch!");  
-             
     }
     if (wurf === pasch && wurf === dreierPasch && pasch === dreierPasch) {
-        alert("Oha, was ein Glück. ")
+        alert("Oha, was ein Glück. Du hast beim ersten Wurf direkt 3 gleiche gewürfelt.")
     }
 
     var wurfAnzahl = document.querySelector<HTMLInputElement>("#wurfAnzahl"); 
@@ -172,11 +159,12 @@ function wuerfeln () {
     var dreierString = dreierPasch.toString();
     dreierAnzahl!.value = dreierString;  
     
-    console.log(wurfString);
-    console.log(paschString);
-    console.log(dreierString);
+    // console.log(wurfString);
+    // console.log(paschString);
+    // console.log(dreierString);
 
     resetButton!.addEventListener('click', reset);
+    
     function reset() {
         wurf = 0;
         pasch = 0;
